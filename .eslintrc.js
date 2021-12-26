b1950f2a61b23e38e418f161fddd0c4291e4dbc0
maoshen5478 <361193645@qq.com>
2021-12-27 00:00:21 +0800

    feat: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,79 +2,86 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
+  parser: 'vue-eslint-parser',
   extends: [
-    'plugin:vue/essential',
-    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
     'plugin:@typescript-eslint/recommended',
-    'standard'
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'eslint:recommended'
   ],
   globals: {
     defineProps: 'readonly',
     withDefaults: 'readonly',
+    module: 'writable',
   },
   parserOptions: {
     ecmaVersion: 13,
     parser: '@typescript-eslint/parser',
     sourceType: 'module'
   },
-  plugins: [
-    'vue',
-    '@typescript-eslint',
-  ],
+  plugins: ['vue', '@typescript-eslint'],
   rules: {
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/ban-types': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    'vue/require-default-prop': 'off',
-    'no-var': 'error', // 禁止使用 var
-    'no-alert': 0,
-    'comma-spacing': 2, // 逗号后空格
-    'semi-spacing': [0, { before: false, after: true }], // 分号后空格
-    '@typescript-eslint/no-var-requires': 0,
-    'array-bracket-spacing': 0, //括号内留空
-    'object-curly-spacing': ['error', 'always'],
-    'no-multi-spaces': 'error', // 禁止多个空格
-    'semi': [2, 'always'],
-    'comma-dangle': ['error', 'only-multiline'],
-    'brace-style': [2, '1tbs', { 'allowSingleLine': true }],
-    'quotes': ['error', 'single'], // 使用单引号
-    'dot-notation':0,
-    'import/first':0,
-    'key-spacing': [0, { 'beforeColon': false, 'afterColon': true }],
-    'vue/singleline-html-element-content-newline':0,
-    'vue/singleline-html-element-content-newline':0,
-    'vue/html-closing-bracket-spacing': 'error',
-    'vue/mustache-interpolation-spacing': 'error',
-    'vue/html-indent': 'error',
-    'space-before-function-paren':0,
-    'no-irregular-whitespace':0,
-    'vue/max-attributes-per-line':['error', {
-      'singleline': {
-        'max': 3,
-        'allowFirstLine': true
-      },
-      'multiline': {
-        'max': 1,
-        'allowFirstLine': false
-      }
-    }],
-    'vue/html-self-closing': 1,
-    'vue/html-self-closing': [
+    '@typescript-eslint/no-unused-vars': [
       'error',
       {
-        html: {
-          void: 'always',
-          normal: 'never',
-          component: 'always'
-        },
-        svg: 'always',
-        math: 'always'
+        argsIgnorePattern: '^h$',
+        varsIgnorePattern: '^h$'
       }
-    ]
+    ],
+    'no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^h$',
+        varsIgnorePattern: '^h$'
+      }
+    ],
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: {
+          max: 3,
+          allowFirstLine: true,
+        },
+        multiline: {
+          max: 1,
+          allowFirstLine: false,
+        }
+      }
+    ],
+    'space-before-function-paren': 'off',
+    quotes: ['error', 'single'],
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'never',
+        objects: 'always',
+        imports: 'always',
+        exports: 'always',
+        functions: 'always'
+      }
+    ],
+    'vue/require-default-prop': 'off',
+    'vue/custom-event-name-casing': 'off',
+    'no-use-before-define': 'off',
+    'no-var': 'error', // 禁止使用 var
+    'vue/comment-directive': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/html-self-closing': 'off',
+    complexity: [2, 9],
+    semi: [2, 'always'],
+    'key-spacing': [0, { beforeColon: false, afterColon: true }],
   }
 }
