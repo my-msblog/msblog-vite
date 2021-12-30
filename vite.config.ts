4c1113760fce9d8c1b40372302d1c955a97db7c7
maoshen5478 <361193645@qq.com>
2021-12-30 18:24:44 +0800

    chore: svg plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,56 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig, ConfigEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import * as path from 'path';
+import viteSvgIcons from 'vite-plugin-svg-icons';
+import { loadEnv } from 'vite';
+import { createProxy } from './build/proxy';
+import { wrapperEnv } from './build/utils';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-        // 配置别名
-        '@': path.resolve(__dirname, './src')
+export default ({ command, mode }: ConfigEnv): UserConfig => {
+  const root = process.cwd();
+
+  const env = loadEnv(mode, root);
+  const viteEnv = wrapperEnv(env);
+  const { VITE_PORT, VITE_APP_BASE_API, VITE_PROXY, VITE_APP_HOST } = viteEnv;
+  return {
+    plugins: [
+      vue(),
+      viteSvgIcons({
+        // 指定需要缓存的图标文件夹
+        iconDirs: [path.resolve(process.cwd(), 'src/styles/icons')],
+        // 指定symbolId格式
+        symbolId: 'icon-[dir]-[name]',
+      }),
+    ],
+    define: {
+      'process.env': {}
+    },
+    resolve: {
+      alias: {
+          // 配置别名
+          '@': path.resolve(__dirname, './src'),
+          '@assets': path.resolve(__dirname,'./src/assets'),
+          '@components': path.resolve(__dirname,'./src/components'),
+          '@views': path.resolve(__dirname,'./src/views'),
+          '@store': path.resolve(__dirname,'./src/store'),
+        }
+    },
+    
+    server: {
+      port: VITE_PORT,
+      host: VITE_APP_HOST,
+      https: false,
+      proxy: {
+        '/api': {
+          // 接口域名
+          target: VITE_APP_BASE_API,
+          // 是否跨域
+          changeOrigin: true,
+          secure: false,
+          rewrite: path => path.replace(/^\/api/, ''),
+        }
       }
+    },
   }
-})
+};
