f1f40f8887b431de80544485ba68bd43daf6ac85
maoshen5478 <361193645@qq.com>
2021-12-30 18:26:01 +0800

    refactor: vite

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <el-button> asd</el-button>
+  <router-view class="root-view" />
 </template>
 <script setup lang="ts"></script>
 
-<style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.root-view{
+  min-height: 650px;
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,32 @@ import App from '@/App.vue';
 // element plus style introduction
 import 'element-plus/theme-chalk/display.css';
 import 'element-plus/dist/index.css';
+// custom animations
+import '@/styles/ann.css';
+// svg global config
+import 'virtual:svg-icons-register';
+import SvgIcon from '@/components/icon/index.vue'
+//import svgLoader from './plugins/svgIcon/svg-loader';
+import router from './router';
+import store from './store';
 import { loadAllPlugins } from '@/plugins/index';
+import { setupI18n } from '@/locales';
+import { registerGlobComp } from '@/components';
 
 (async()=>{
    const app =  createApp(App);
+   // svg plugin
+   app.component('SvgIcon', SvgIcon);
+   // Multilingual configuration
+   setupI18n(app);
+   // Load plugins
    await loadAllPlugins(app);
-    
+    // Register global components
+   registerGlobComp(app);
+   // router ready https://next.router.vuejs.org/api/#isready
+   app.use(router);
+   // await router.isReady();
+   app.use(store);
    app.mount('#app');
 })();
 
