53ae01b576a1fa877264f22ecafe564dade7ba4c
maoshen5478 <361193645@qq.com>
2022-01-01 00:24:20 +0800

    chore: proxy configuration

---
+++ types/global.d.ts
@@ -0,0 +1,15 @@
+export {};
+declare global {
+    declare type Nullable<T> = T | null;
+    declare interface ViteEnv {
+        VITE_PORT: number;
+        NODE_ENV: string;
+        VITE_BUILD_ENV: string;
+        VITE_PUBLIC_PATH: string;
+        VITE_PROXY: [string, string][];
+        VITE_GLOB_APP_TITLE: string;
+        VITE_GLOB_APP_SHORT_NAME: string;
+        VITE_APP_HOST: string;
+        VITE_BUILD_COMPRESS: 'gzip' | 'brotli' | 'none';
+    }
+}
