ac3419882f87b021cdb6861229290950dba99d84
maoshen5478 <361193645@qq.com>
2022-01-02 01:24:44 +0800

    fix: vite router can`t be import

--- src/layout/admin/index.vue
+++ src/layout/admin/index.vue
@@ -6,7 +6,7 @@
     <el-container :class="{'folded':data.folded}">
       <el-aside class="ly_left">
         <el-scrollbar>
-          <PermissionMenu class="ly_menu" :menu="data.menu" :isCollapse="data.folded" />
+          <PermissionMenu class="ly_menu" :menu="data.menu" :is-collapse="data.folded" />
         </el-scrollbar>
         <span class="btn_folded" @click="data.folded = !data.folded">
           <i :class="data.folded ? 'el-icon-s-fold' : 'el-icon-s-unfold'" style="color: #888" />
