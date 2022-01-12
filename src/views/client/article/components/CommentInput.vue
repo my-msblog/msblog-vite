369d203dc1e42110ff025cd7cd3ff0abe3b26725
maoshen5478 <361193645@qq.com>
2022-01-12 23:26:38 +0800

    wip: article page

---
+++ src/views/client/article/components/CommentInput.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="com">
+    <div></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'CommentInput',
+  setup() {
+    return {};
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/client/article/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    Template
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from 'vue';
+import { useRouter } from 'vue-router';
+export default defineComponent({
+  name: 'Article',
+  setup() {
+    const router = useRouter();
+    const articleId = router.currentRoute.value.params.id;
+    const data = reactive({
+        commentList: [],
+    });
+    const handleArticle = () =>{
+        // get article api
+    };
+    return {
+      data,
+    };
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
