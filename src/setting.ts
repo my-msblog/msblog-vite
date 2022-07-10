a4cf28e0d5370a2323d8db2abf9e583b6a8ffb9f
maoshen5478 <361193645@qq.com>
2022-07-10 21:27:31 +0800

    feat:

--- src/setting.ts
+++ src/setting.ts
@@ -1,10 +1,10 @@
 // cos 密钥验证
 export const cos = {
-  ID: 'AKIDPdbJkunQmrfCTTCepjMinb8OM4VfYAsJ',
-  KEY: 'A4h37uQfdKlvnOilX2FCtIQHr9BwRfcI',
-  bucket: 'ms-1258904389',
-  region: 'ap-nanjing',
+  ID: '',
+  KEY: '',
+  bucket: '',
+  region: '',
 };
 
 // 加密传输
-export const secret = { id: '1234567890123456', key: '1234567890123456' };
+export const secret = { id: '', key: '' };
